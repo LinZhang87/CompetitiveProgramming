a92605db935650f3c659238fe113a60f75a37fa6
Lin <lyn.linzhang@gmail.com>
2021-05-02 14:06:11 -0600

    Create B.java

---
+++ contests/codeforces/global14/B.java
@@ -0,0 +1,139 @@
+package codeforces.global14;
+
+import java.io.*;
+import java.util.*;
+
+/*
+    we can only use 2 or 4 pieces to construct a sub-square, so n must be divisible by 2;
+    In order to have a square, we must have k * k sub-squares; Denote the largest factor of 
+    n that is not divisible by 2 as V, then V must be a perfect square number, i.e, V = k * k.
+ */
+
+public class B {
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        //initReaderPrinter(true);
+        initReaderPrinter(false);
+        solve(in.nextInt());
+        //solve(1);
+    }
+
+    static void solve(int testCnt) {
+        for (int testNumber = 0; testNumber < testCnt; testNumber++) {
+            int n = in.nextInt();
+            if(n % 2 != 0) out.println("NO");
+            else {
+                while(n % 2 == 0) {
+                    n /= 2;
+                }
+                int v = (int)Math.sqrt(n);
+                out.println(v * v == n ? "YES" : "NO");
+            }
+        }
+        out.close();
+    }
+
+    static void initReaderPrinter(boolean test) {
+        if (test) {
+            try {
+                in = new InputReader(new FileInputStream("src/input.in"));
+                out = new PrintWriter(new FileOutputStream("src/output.out"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            in = new InputReader(System.in);
+            out = new PrintWriter(System.out);
+        }
+    }
+
+    static class InputReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        InputReader(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream), 32768);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        Integer[] nextIntArray(int n) {
+            Integer[] a = new Integer[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[] nextIntArrayPrimitive(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[] nextIntArrayPrimitiveOneIndexed(int n) {
+            int[] a = new int[n + 1];
+            for (int i = 1; i <= n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        Long[] nextLongArray(int n) {
+            Long[] a = new Long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long[] nextLongArrayPrimitive(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long[] nextLongArrayPrimitiveOneIndexed(int n) {
+            long[] a = new long[n + 1];
+            for (int i = 1; i <= n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        String[] nextStringArray(int n) {
+            String[] g = new String[n];
+            for (int i = 0; i < n; i++) g[i] = next();
+            return g;
+        }
+    }
+}
