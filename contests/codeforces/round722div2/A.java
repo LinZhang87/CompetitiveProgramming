dd78de3a6dac3c153e8fe3e2dbab55ba31428961
Lin <lyn.linzhang@gmail.com>
2021-05-25 20:35:28 -0600

    Create A.java

---
+++ contests/codeforces/round722div2/A.java
@@ -0,0 +1,141 @@
+package codeforces.round722div2;
+
+import java.io.*;
+import java.util.*;
+
+/*
+    Only the min value can not be deleted, so the max delete count is N - cnt(min value)
+ */
+
+public class A {
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        //initReaderPrinter(true);
+        initReaderPrinter(false);
+        solve(in.nextInt());
+        //solve(1);        
+    }
+
+    static void solve(int testCnt) {
+        for (int testNumber = 0; testNumber < testCnt; testNumber++) {
+            int n = in.nextInt();
+            int[] cnt = new int[101];
+            for(int i = 0; i < n; i++) {
+                int v = in.nextInt();
+                cnt[v]++;
+            }
+            int j = 1;
+            for(; j <= 100; j++) {
+                if(cnt[j] > 0) {
+                    break;
+                }
+            }
+            out.println(n - cnt[j]);
+        }
+        out.close();
+    }
+
+    static void initReaderPrinter(boolean test) {
+        if (test) {
+            try {
+                in = new InputReader(new FileInputStream("src/input.in"));
+                out = new PrintWriter(new FileOutputStream("src/output.out"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            in = new InputReader(System.in);
+            out = new PrintWriter(System.out);
+        }
+    }
+
+    static class InputReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        InputReader(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream), 32768);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        Integer[] nextIntArray(int n) {
+            Integer[] a = new Integer[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[] nextIntArrayPrimitive(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        int[] nextIntArrayPrimitiveOneIndexed(int n) {
+            int[] a = new int[n + 1];
+            for (int i = 1; i <= n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        Long[] nextLongArray(int n) {
+            Long[] a = new Long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long[] nextLongArrayPrimitive(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long[] nextLongArrayPrimitiveOneIndexed(int n) {
+            long[] a = new long[n + 1];
+            for (int i = 1; i <= n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        String[] nextStringArray(int n) {
+            String[] g = new String[n];
+            for (int i = 0; i < n; i++) g[i] = next();
+            return g;
+        }
+    }
+}
